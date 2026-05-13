5ea1a1dfdc415f1db8650e9734c6473ec55657a4
Rhajaina Agent <dev@stromdao.com>
2026-05-13 23:42:30 +0000

    fix: normalize API base URL to prevent double /api/ paths
    - Add automatic .replace(/\/api\/$/, ...) on baseUrl
    - Prevents https://api.cernion.de/api/api/edm/melos when user enters /api/ suffix
    - ES5 compatible, no template literals

--- api.js
+++ api.js
@@ -115,6 +115,7 @@ var CERNION_CONFIG_KEY = 'cernion.api.config';
 class CernionAPI {
   constructor() {
     this.config = this.loadConfig();
+    this.config.baseUrl = (this.config.baseUrl || 'https://api.cernion.de/').replace(/\/api\/$/, '');
   }
   loadConfig() {
     try {
